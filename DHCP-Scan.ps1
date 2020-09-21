4729655e2bf6ba4c6ce5db9aaede6f7f009e35d8
zubairmk83 <65172270+zubairmk83@users.noreply.github.com>
2020-09-21 14:36:30 +0200

    Powershell script to query DHCP lease
    Script should be run in DC or on a machine that has powershell domain controller module imported. It will scan for existing DHCP servers in domain and then scan through the respective servers to populate leases utilized. It will through errors if DHCP server is unreachable or if Domain account does not have permission.

---
+++ DHCP-Scan.ps1
@@ -0,0 +1,34 @@
+﻿#######################################
+# By: Zubair Khan                     #
+#     YMA-SA - DSMA                   #
+#     21/9/2020                       #
+#######################################
+
+# Note:
+# The Script needs to be run on Windows Domain controllers
+
+
+# We will first collect all DHCP instances in your domain
+$dhcpServers = Get-DhcpServerInDC | Select DnsName
+
+ 
+# Now we are going to scan all scopes per DHCP detected. This may throw a few errors depending on DHCP server reachability and permissions
+# The specified queries are all Get commands and will not make changes on your Domain controller
+
+foreach($ser in $dhcpServers){
+    if(Test-Connection -ComputerName $ser.DnsName -Count 2 -Quiet -ErrorAction SilentlyContinue){
+        $scope = Get-DhcpServerv4Scope -ComputerName $ser.DnsName
+        #$scope
+        foreach($s in $scope.ScopeId){
+        $SID += Get-DhcpServerv4Lease -ComputerName $ser.DnsName -ScopeId $s | select AddressState, ClientID, IPAddress, ScopeID, Hostname, LeaseExpiryTime, ServerIP, @{Name="Server";Expression={$ser.DnsName}}
+        }
+
+
+    }
+}
+
+# The result can be exported in a CSV format to be processed in Excel. 
+$SID | Export-Csv -Path C:\temp\DHCPOutput.csv
+
+# The result can be presented in a tableview which can be expeorted to an excel or CSV file
+$SID | Out-GridView
